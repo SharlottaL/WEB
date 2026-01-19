ac268af58ea7d7f18fd740a0658bf4012dfb72b3
SharlottaL <botanichkagigantika@gmail.com>
2026-01-19 15:30:13 +0300

    power and fibonach DONE

--- JavaScript/intro/script.js
+++ JavaScript/intro/script.js
@@ -15,3 +15,26 @@ function Factorial2()
     let factorial_result = document.getElementById("factorial-result");
     factorial_result.innerHTML = Factorial(source_value);
 }
+
+function Power()
+{
+    let osnov_input_field = document.getElementById("power-source-osnov");
+    let osnov_value = osnov_input_field.value;
+    let step_input_field = document.getElementById("power-source-step");
+    let step_value = step_input_field.value;
+    let power_result = document.getElementById("power-result");
+    power_result.innerHTML = (osnov_value ** step_value);
+}
+function Fibonachi()
+{
+    let fibonachi_input_field = document.getElementById("fibonachi-source");
+    let fibonachi_value = fibonachi_input_field.value;
+    let fibonachi_result = document.getElementById("fibonachi-result");
+    let a = 0, b = 1;
+    let fibonachi_numbers = [];
+    while (a <= fibonachi_value) {
+    fibonachi_numbers.push(a);
+    [a, b] = [b, a + b]; 
+}
+  fibonachi_result.innerHTML = fibonachi_numbers.join(',');
+}
